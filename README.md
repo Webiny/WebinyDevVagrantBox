80cbe363ba7b6292c531fc5bc4b9851be240ea48
sven <sven@webiny.com>
2016-01-10 14:51:07 +0000

    box init

---
+++ README.md
@@ -0,0 +1,18 @@
+Webiny dev
+==========
+
+Vagrant dev box for Webiny Platform 
+
+## Version 0.1.0
+- PHP v7.0.2
+- MongoDb v3.2.0
+- NGINX v1.4.6
+- Git 1.9.1
+- PHPUnit v5.1.3
+- Composer v1.0-dev (alpha 11)
+
+PHP extensions included
+- php7-gd
+- php7-mcrypt
+- php7-memcache
+- mongodb
---
+++ scripts/webiny-dev.rb
@@ -0,0 +1,112 @@
+class WebinyDev
+  def WebinyDev.configure(config, settings)
+
+    # Configure The Box
+    config.vm.box = settings["box"] ||= "webiny-dev"
+    config.vm.box_version = settings["version"] ||= ">= 0.1.0"
+    config.vm.hostname = settings["hostname"] ||= "webiny-dev"
+
+    # Allow SSH Agent Forward from The Box
+    config.ssh.forward_agent = true
+
+    # Configure A Private Network IP
+    config.vm.network :private_network, ip: settings["ip"] ||= "192.168.22.22"
+
+    if settings['networking'][0]['public']
+      config.vm.network "public_network", type: "dhcp"
+    end
+
+    # Configure A Few VirtualBox Settings
+    config.vm.provider "virtualbox" do |vb|
+      vb.name = settings["name"] ||= "webiny-dev"
+      vb.customize ["modifyvm", :id, "--memory", settings["memory"] ||= "2048"]
+      vb.customize ["modifyvm", :id, "--cpus", settings["cpus"] ||= "1"]
+      vb.customize ["modifyvm", :id, "--natdnsproxy1", "on"]
+      vb.customize ["modifyvm", :id, "--natdnshostresolver1", "on"]
+      vb.customize ["modifyvm", :id, "--ostype", "Ubuntu_64"]
+    end
+
+    # Configure A Few VMware Settings
+    ["vmware_fusion", "vmware_workstation"].each do |vmware|
+      config.vm.provider vmware do |v|
+        v.vmx["displayName"] = "webiny-dev"
+        v.vmx["memsize"] = settings["memory"] ||= 2048
+        v.vmx["numvcpus"] = settings["cpus"] ||= 1
+        v.vmx["guestOS"] = "ubuntu-64"
+      end
+    end
+
+    # Configure A Few Parallels Settings
+    config.vm.provider "parallels" do |v|
+      v.update_guest_tools = true
+      v.memory = settings["memory"] ||= 2048
+      v.cpus = settings["cpus"] ||= 1
+    end
+
+    # Standardize Ports Naming Schema
+    if (settings.has_key?("ports"))
+      settings["ports"].each do |port|
+        port["guest"] ||= port["to"]
+        port["host"] ||= port["send"]
+        port["protocol"] ||= "tcp"
+      end
+    else
+      settings["ports"] = []
+    end
+
+    # Default Port Forwarding
+    default_ports = {
+      80   => 8000,
+      443  => 44300,
+      3306 => 33060,
+      5432 => 54320
+    }
+
+    # Use Default Port Forwarding Unless Overridden
+    default_ports.each do |guest, host|
+      unless settings["ports"].any? { |mapping| mapping["guest"] == guest }
+        config.vm.network "forwarded_port", guest: guest, host: host, auto_correct: true
+      end
+    end
+
+    # Add Custom Ports From Configuration
+    if settings.has_key?("ports")
+      settings["ports"].each do |port|
+        config.vm.network "forwarded_port", guest: port["guest"], host: port["host"], protocol: port["protocol"], auto_correct: true
+      end
+    end
+
+    # Configure The Public Key For SSH Access
+    if settings.include? 'authorize'
+      if File.exists? File.expand_path(settings["authorize"])
+        config.vm.provision "shell" do |s|
+          s.inline = "echo $1 | grep -xq \"$1\" /home/vagrant/.ssh/authorized_keys || echo $1 | tee -a /home/vagrant/.ssh/authorized_keys"
+          s.args = [File.read(File.expand_path(settings["authorize"]))]
+        end
+      end
+    end
+
+    # Copy The SSH Private Keys To The Box
+    if settings.include? 'keys'
+      settings["keys"].each do |key|
+        config.vm.provision "shell" do |s|
+          s.privileged = false
+          s.inline = "echo \"$1\" > /home/vagrant/.ssh/$2 && chmod 600 /home/vagrant/.ssh/$2"
+          s.args = [File.read(File.expand_path(key)), key.split('/').last]
+        end
+      end
+    end
+
+    # Register All Of The Configured Shared Folders
+    if settings['folders'].kind_of?(Array)
+      settings["folders"].each do |folder|
+        config.vm.synced_folder folder["map"], folder["to"], type: folder["type"] ||= nil
+      end
+    end
+
+    # Update Composer On Every Provision
+    config.vm.provision "shell" do |s|
+      s.inline = "/usr/local/bin/composer self-update --no-progress"
+    end
+  end
+end
